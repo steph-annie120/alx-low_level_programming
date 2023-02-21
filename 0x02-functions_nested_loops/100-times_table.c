ac3f9c86cc8c891c7a32f0982e7fc14485de86bc
steph-annie120 <ostephaniemaryogbonna@gmail.com>
2023-02-21 11:37:36 -0800

    Task  The World looks like a multiplication-table, or a mathematical equation, which, turn it how you will, balances itself

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * print_times_table - prints the times table
+ * @n: integer for which the times table will be printed
+ *
+ * Return: Always 0 (Success)
+ * Author: IanoNjuguna
+ */
+
+void print_times_table(int n)
+{
